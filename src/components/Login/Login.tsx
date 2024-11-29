05f8ff818b6f8ecfaee6c5f8248755ff9c5a3b8e
Parth Patel <you@example.com>
2024-11-29 18:04:00 +0530

    added: singup feature

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,81 @@
+import styles from './Login.module.css';
+import { ILogin } from './login.model';
+import { FormEvent, useState } from 'react';
+import { login } from '../../services/Customers.service';
+
 function Login() {
+  const initialFields: ILogin = {
+    email: "",
+    password: ""
+  };
+
+  const [formInputs, setFormInputs] = useState(initialFields);
+  const [formFieldErrors, setFormFieldErrors] = useState<Partial<ILogin>>({});
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if(validateForm()) {
+        const response = await login(formInputs);
+        if(response.error) {
+            alert(response.error);
+        } else {
+            alert("User logged in.");
+            setFormInputs(initialFields);
+        }    
+    }
+  }
+
+  const validateForm = () => {
+    const errors: Partial<ILogin> = {};
+    if(!formInputs.email) errors.email = "Email is required!";
+    if(!formInputs.password) errors.password = "Password is required!";
+
+    setFormFieldErrors(errors);
+    return Object.keys(errors).length === 0;
+}
+
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleSubmit}>
+        <div className={styles.container}>
+            <h1>Login</h1>
+            <hr />
+            <div className={styles.formGroup}>
+              <label htmlFor='email' className={styles.formInputLabel}>Email</label>
+              <input
+                  type='text'
+                  id='email'
+                  name='email'
+                  className={styles.formInput}
+                  onChange={(e) => setFormInputs((prev) => ({
+                    ...prev, email: e.target.value
+                  }))}
+              />
+              <p className={styles.formValidationText}>
+              {formFieldErrors.email}
+              </p>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor='password' className={styles.formInputLabel}>Password</label>
+              <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  className={styles.formInput}
+                  onChange={(e) => setFormInputs((prev) => ({
+                    ...prev, password: e.target.value
+                  }))}
+              />
+              <p className={styles.formValidationText}>
+                {formFieldErrors.password}
+              </p>
+            </div>
+            
+            <button type='submit' className={styles.loginButton}>
+                Login
+            </button>
+        </div>
+    </form>
+);
 }
 
 export default Login
--- src/services/Customers.service.ts
+++ src/services/Customers.service.ts
@@ -1,5 +1,6 @@
 import { API_URL } from "../configs/config";
 import { ICustomer } from '../components/Register/register.model'
+import { ILogin } from '../components/Login/login.model';
 
 const registerCustomer = async (customer: ICustomer) => {
     const customerData = {
@@ -19,4 +20,19 @@ const registerCustomer = async (customer: ICustomer) => {
     }).then((response) => response.json());
 };
 
-export { registerCustomer };
+const login = async (login: ILogin) => {
+    const loginCreds = {
+        "email": login.email,
+        "password": login.password
+    };
+    return await fetch(`${API_URL}/customers/signin`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(loginCreds)
+    }).then((response) => response.json());
+}
+
+export { registerCustomer, login };
